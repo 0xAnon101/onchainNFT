21655ede5ebfa66cb11f9fd15db451c5d2c21d03
Raju <rajugautam45@gmail.com>
2021-10-16 16:27:28 +0530

    updated contract functions

--- contracts/RariNFT.sol
+++ contracts/RariNFT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -12,14 +11,23 @@ contract RariNFT is ERC721URIStorage {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
+  
+  string _baseTokenURI;
   uint256 public MAX_MINT_LIMIT = 50;
+  uint256 internal _price = 0.05 ether;
+  address public owner;
   mapping(uint256 => address) rariIndexToAddress;
   mapping (address => uint256) public _balanceOf;
 
   event NewRareNFTMinted(address _sender, uint256 _tokenId);
 
   constructor() ERC721 ("RariNFT", "RARI") {
-    console.log("First NFT!");
+    owner = msg.sender;
+  }
+
+  modifier OnlyOwner {
+    require(msg.sender == owner, "Sender is not owner!");
+    _;
   }
 
   function getTotalNFTMintedSoFar() public view returns (uint256) {
@@ -30,25 +38,42 @@ contract RariNFT is ERC721URIStorage {
     return _balanceOf[msg.sender];
   }
 
-  function pickRandomFirstWord(uint256 tokenId, string[9] memory firstWords) public pure returns (string memory) {
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseTokenURI;
+  }
+
+  function _setBaseTokenURI(string memory _uri) public OnlyOwner {
+    _baseTokenURI = _uri;
+  }
+   
+  function setPrice(uint256 _newPrice) public OnlyOwner {
+    _price = _newPrice;
+  }
+  
+  function getPrice() public view returns (uint256){
+     return _price;
+  }
+
+
+  function _pickRandomFirstWord(uint256 tokenId, string[9] memory firstWords) internal pure returns (string memory) {
       uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
       rand = rand % firstWords.length;
       return firstWords[rand];
   }
 
-  function pickRandomSecondWord(uint256 tokenId, string[8] memory secondWords) public pure returns (string memory) {
+  function _pickRandomSecondWord(uint256 tokenId, string[8] memory secondWords) internal pure returns (string memory) {
     uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
     rand = rand % secondWords.length;
     return secondWords[rand];
   }
 
-  function pickRandomThirdWord(uint256 tokeId, string[9] memory thirdWords) public pure returns(string memory) {
+  function _pickRandomThirdWord(uint256 tokeId, string[9] memory thirdWords) internal pure returns(string memory) {
     uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokeId))));
     rand = rand % thirdWords.length;
     return thirdWords[rand];
   }
 
-  function pickRandomColor(uint256 tokenId, string[6] memory colors) public pure returns (string memory) {
+  function _pickRandomColor(uint256 tokenId, string[6] memory colors) internal pure returns (string memory) {
     uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
     rand = rand % colors.length;
     return colors[rand];
@@ -58,13 +83,13 @@ contract RariNFT is ERC721URIStorage {
       return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  function combineWords(uint256 newItemId) public pure returns (string memory){
+  function _combineWords(uint256 newItemId) internal pure returns (string memory){
     string[9] memory firstWords = ["Saitama", "Goku", "Vegeta", "Luffy", "Ichigo", "Aomine", "Kira", "Naruto", "Deku"];
     string[8] memory secondWords = ["Slaps", "Punches", "Kicks", "Kills", "Loves", "Hates", "Envy", "Stares"];
     string[9] memory thirdWords = ["Lion", "Hyena", "Tiger", "Snake", "Elephant", "Cheetah", "Monkey", "Chimp", "Eagle"];
-    string memory first = pickRandomFirstWord(newItemId, firstWords);
-    string memory second = pickRandomSecondWord(newItemId, secondWords);
-    string memory third = pickRandomThirdWord(newItemId, thirdWords);
+    string memory first = _pickRandomFirstWord(newItemId, firstWords);
+    string memory second = _pickRandomSecondWord(newItemId, secondWords);
+    string memory third = _pickRandomThirdWord(newItemId, thirdWords);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
     return combinedWord;
@@ -74,8 +99,8 @@ contract RariNFT is ERC721URIStorage {
     string memory svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
     string memory svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
     string[6] memory colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
-    string memory combinedWord = combineWords(newItemId);
-    string memory randomColor = pickRandomColor(newItemId, colors);
+    string memory combinedWord = _combineWords(newItemId);
+    string memory randomColor = _pickRandomColor(newItemId, colors);
     string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
   
     string memory json = Base64.encode(
@@ -87,7 +112,7 @@ contract RariNFT is ERC721URIStorage {
             '"}'
           ))));
     string memory finalTokenURI =  string(abi.encodePacked("data:application/json;base64, ", json));
-    
+
     return finalTokenURI;
   }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
@@ -8,5 +9,8 @@ module.exports = {
       url: process.env.ALCHEMY_TESTNET_API_URL,
       accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.RINKEBY_ETHERSCAN_KEY,
   },
 };
